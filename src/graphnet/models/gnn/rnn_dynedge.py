4b3c33a3d0b9b38fef18a44ef3e8f508f11bf7c2
askerosted@gmail.com <askerosted@gmail.com>
2024-01-22 11:33:54 +0900

    Adding more features

--- src/graphnet/models/gnn/rnn_dynedge.py
+++ src/graphnet/models/gnn/rnn_dynedge.py
@@ -6,30 +6,37 @@ from graphnet.models.gnn.gnn import GNN
 from graphnet.models.gnn.dynedge import DynEdge
 from graphnet.models.gnn.dynedge_kaggle_tito import DynEdgeTITO
 from graphnet.models.rnn.node_rnn import Node_RNN
-from graphnet.models.rnn.dom_window_rnn import Dom_Window_RNN
+
+# from graphnet.models.rnn.dom_window_rnn import Dom_Window_RNN
+from graphnet.models.rnn.node_transformer import Node_Transformer
 
 from graphnet.utilities.config import save_model_config
 from torch_geometric.data import Data
 
 
 class RNN_DynEdge(GNN):
-    """The RNN_DynEdge model class."""
+    """The RNN_DynEdge model class.
+
+    works only with 2 dimensional time series of (charge, time)
+    """
 
     @save_model_config
     def __init__(
         self,
         nb_inputs: int,
         *,
-        # nb_neighbours: int = 8,
+        nb_neighbours: int = 8,
         RNN_layers: int = 2,
         RNN_hidden_size: int = 64,
         RNN_dropout: float = 0.5,
         features_subset: Optional[Union[List[int], slice]] = None,
-        dynedge_layer_sizes: Optional[List[Tuple[int, ...]]] = None,
-        # post_processing_layer_sizes: Optional[List[int]] = None,
+        dyntrans_layer_sizes: Optional[List[Tuple[int, ...]]] = None,
+        post_processing_layer_sizes: Optional[List[int]] = None,
         readout_layer_sizes: Optional[List[int]] = None,
         global_pooling_schemes: Optional[Union[str, List[str]]] = None,
         # add_global_variables_after_pooling: bool = False,
+        embedding_dim: Optional[int] = None,
+        n_head: int = 16,
     ):
         """Initialize the RNN_DynEdge model.
 
@@ -41,42 +48,38 @@ class RNN_DynEdge(GNN):
                 Defaults to 1.
             RNN_hidden_size (int, optional): Size of the hidden state of the RNN. Also determines the size of the output of the RNN.
                 Defaults to 64.
+            RNN_dropout (float, optional): Dropout to use in the RNN. Defaults  to 0.5.
             features_subset (Optional[Union[List[int], slice]], optional): Subset of features to use.
-            dynedge_layer_sizes (Optional[List[Tuple[int, ...]]], optional): List of tuples of integers representing the sizes of the hidden layers of the DynEdge model.
+            dyntrans_layer_sizes (Optional[List[Tuple[int, ...]]], optional): List of tuples representing the sizes of the hidden layers of the DynTrans model.
             post_processing_layer_sizes (Optional[List[int]], optional): List of integers representing the sizes of the hidden layers of the post-processing model.
             readout_layer_sizes (Optional[List[int]], optional): List of integers representing the sizes of the hidden layers of the readout model.
             global_pooling_schemes (Optional[Union[str, List[str]]], optional): Pooling schemes to use. Defaults to None.
-            add_global_variables_after_pooling (bool, optional): Whether to add global variables after pooling. Defaults to False.
+            embedding_dim (Optional[int], optional): Embedding dimension of the RNN. Defaults to None ie. no embedding.
+            n_head (int, optional): Number of heads to use in the DynTrans model. Defaults to 16.
         """
-        # self._nb_neighbours = nb_neighbours
+        self._nb_neighbours = nb_neighbours
         self._nb_inputs = nb_inputs
         self._RNN_layers = RNN_layers
         self._RNN_hidden_size = RNN_hidden_size  # RNN_hidden_size
         self._RNN_dropout = RNN_dropout
+        self._embedding_dim = embedding_dim
+        self._n_head = n_head
 
         self._features_subset = features_subset
-        # if dynedge_layer_sizes is None:
-        #     dynedge_layer_sizes = [
-        #         (
-        #             256,
-        #             512,
-        #         ),
-        #         (
-        #             512,
-        #             1024,
-        #         ),
-        #         (
-        #             1024,
-        #             512,
-        #         ),
-        #         (
-        #             512,
-        #             256,
-        #         ),
-        #     ]
+        if dyntrans_layer_sizes is None:
+            dyntrans_layer_sizes = [
+                (256, 256),
+                (256, 256),
+                (256, 256),
+                (256, 256),
+            ]
+        else:
+            dyntrans_layer_sizes = [
+                tuple(layer_sizes) for layer_sizes in dyntrans_layer_sizes
+            ]
 
-        self._dynedge_layer_sizes = dynedge_layer_sizes
-        # self._post_processing_layer_sizes = post_processing_layer_sizes
+        self._dyntrans_layer_sizes = dyntrans_layer_sizes
+        self._post_processing_layer_sizes = post_processing_layer_sizes
         self._global_pooling_schemes = global_pooling_schemes
         # self._add_global_variables_after_pooling = (
         #     add_global_variables_after_pooling
@@ -96,7 +99,15 @@ class RNN_DynEdge(GNN):
             hidden_size=self._RNN_hidden_size,
             # nb_neighbours=self._nb_neighbours,
             RNN_dropout=self._RNN_dropout,
+            embedding_dim=self._embedding_dim,
         )
+
+        # self._node_transformer = Node_Transformer(
+        #     nb_inputs=2,
+        #     hidden_size=self._RNN_hidden_size,
+        #     RNN_dropout=self._RNN_dropout,
+        #     num_layers=self._RNN_layers,
+        # )
 
         # self._rnn = Dom_Window_RNN(
         #     num_layers=self._RNN_layers,
@@ -117,14 +128,21 @@ class RNN_DynEdge(GNN):
         self._dynedge_tito = DynEdgeTITO(
             nb_inputs=self._RNN_hidden_size + 5,
             # nb_neighbours=self._nb_neighbours,
-            # dyntrans_layer_sizes=self._dynedge_layer_sizes,
+            dyntrans_layer_sizes=self._dyntrans_layer_sizes,
             features_subset=self._features_subset,
             global_pooling_schemes=self._global_pooling_schemes,
+            use_global_features=True,
+            use_post_processing_layers=True,
+            post_processing_layer_sizes=self._post_processing_layer_sizes,
+            readout_layer_sizes=self._readout_layer_sizes,
+            n_head=self._n_head,
+            nb_neighbours=self._nb_neighbours,
         )
 
     def forward(self, data: Data) -> torch.Tensor:
         """Apply learnable forward pass of the RNN and DynEdge models."""
         data = self._rnn(data)
+        # data = self._node_transformer(data)
         readout = self._dynedge_tito(data)
 
         return readout
--- src/graphnet/models/rnn/node_rnn.py
+++ src/graphnet/models/rnn/node_rnn.py
@@ -7,6 +7,7 @@ import torch
 from graphnet.models.gnn.gnn import GNN
 from graphnet.utilities.config import save_model_config
 from torch_geometric.data import Data
+from typing import Optional
 
 
 class Node_RNN(GNN):
@@ -24,6 +25,7 @@ class Node_RNN(GNN):
         hidden_size: int,
         num_layers: int,
         RNN_dropout: float = 0.5,
+        embedding_dim: int = 0,
     ) -> None:
         """Construct `NodeTimeRNN`.
 
@@ -32,11 +34,18 @@ class Node_RNN(GNN):
             hidden_size: Number of features for the RNN output and hidden layers.
             num_layers: Number of layers in the RNN.
             nb_neighbours: Number of neighbours to use when reconstructing the graph representation.
+            RNN_dropout: Dropout fractio to use in the RNN. Defaults to 0.5.
+            embedding_dim: Embedding dimension of the RNN. Defaults to no embedding.
         """
         self._num_layers = num_layers
         self._hidden_size = hidden_size
+        self._embedding_dim = embedding_dim
         self._nb_inputs = nb_inputs
+
         super().__init__(nb_inputs, hidden_size + 5)
+
+        if self._embedding_dim != 0:
+            self._nb_inputs = self._embedding_dim * 2 * nb_inputs
 
         self._rnn = torch.nn.GRU(
             num_layers=self._num_layers,
@@ -45,12 +54,23 @@ class Node_RNN(GNN):
             batch_first=True,
             dropout=RNN_dropout,
         )
+        self._emb = SinusoidalPosEmb(dim=self._embedding_dim)
 
     def forward(self, data: Data) -> torch.Tensor:
         """Apply learnable forward pass to the GNN."""
         cutter = data.cutter.cumsum(0)[:-1]
+        if self._embedding_dim != 0:
+            time_series = self._emb(data.time_series * 4096).reshape(
+                (
+                    data.time_series.shape[0],
+                    self._embedding_dim * 2 * data.time_series.shape[-1],
+                )
+            )
+        else:
+            time_series = data.time_series
+
         time_series = torch.nn.utils.rnn.pack_sequence(
-            data.time_series.tensor_split(cutter.cpu()), enforce_sorted=False
+            time_series.tensor_split(cutter.cpu()), enforce_sorted=False
         )
 
         rnn_out = self._rnn(time_series)[-1][0]
@@ -70,3 +90,28 @@ class Node_RNN(GNN):
             [data.x, rnn_out]
         )  # reintroduce x/y/z-coordinates and time of first/mean activation for each DOM
         return data
+
+
+class SinusoidalPosEmb(torch.nn.Module):
+    """Sinusoidal positional embedding layer."""
+
+    def __init__(self, dim: int = 16, M: int = 10000) -> None:
+        """Construct `SinusoidalPosEmb`.
+
+        Args:
+            dim: Embedding dimension.
+            M: Number of frequencies.
+        """
+        super().__init__()
+        self.dim = dim
+        self.M = M
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """Apply learnable forward pass to the layer."""
+        device = x.device
+        half_dim = self.dim
+        emb = torch.log(torch.tensor(self.M, device=device)) / half_dim
+        emb = torch.exp(torch.arange(half_dim, device=device) * (-emb))
+        emb = x[..., None] * emb[None, ...]
+        emb = torch.cat((emb.sin(), emb.cos()), dim=-1)
+        return emb
