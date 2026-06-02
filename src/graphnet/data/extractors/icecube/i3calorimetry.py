3da19af1a635bd956fdc0581dc0264e4e17d3a37
askerosted@gmail.com <askerosted@gmail.com>
2026-06-02 02:01:21 -0500

    a little leniency

--- src/graphnet/data/extractors/icecube/i3calorimetry.py
+++ src/graphnet/data/extractors/icecube/i3calorimetry.py
@@ -69,7 +69,7 @@ class I3Calorimetry(I3Extractor):
             )
             if len(target_tree) + len(bkg_tree) != len(frame[self.mctree]):
                 raise ValueError(
-                    "Split mctree has different number of particles than original mctree"
+                    f"Split mctree has different number of particles than original mctree\nOriginal mctree: {len(frame[self.mctree])}\nTarget tree: {len(target_tree)}\nBkg tree: {len(bkg_tree)}\nHighest energy primary flag: {self.highest_energy_primary}\nEvent header: {frame['I3EventHeader']}"
                 )
 
             # For the target we consider either all neutrino primary products or only the highest energy primary of the neutrino depending on the flag.
@@ -95,7 +95,7 @@ class I3Calorimetry(I3Extractor):
             )
             # Sanity check ensuring no double counting
 
-            if e_track_target > target_primaries_energy:
+            if not (e_track_target <= target_primaries_energy * (1 + 1e-6)):
                 raise ValueError(
                     f"Energy deposited in target is greater than primary energy: {e_track_target} > {target_primaries_energy}\nEvent header: {frame['I3EventHeader']}"
                 )
@@ -103,7 +103,7 @@ class I3Calorimetry(I3Extractor):
                 e_track_bkg = self.total_track_energy(
                     frame, bkg_tree, entrance_energy=self.entrance_energy
                 )
-                if e_track_bkg > bkg_primaries_energy:
+                if not (e_track_bkg <= bkg_primaries_energy * (1 + 1e-6)):
                     raise ValueError(
                         f"Energy deposited in background is greater than primary energy: {e_track_bkg} > {bkg_primaries_energy}\nEvent header: {frame['I3EventHeader']}"
                     )
@@ -113,7 +113,10 @@ class I3Calorimetry(I3Extractor):
             e_cascade_target = self.total_cascade_energy(
                 target_tree, target_primaries
             )
-            if e_cascade_target > target_primaries_energy:
+            if not (
+                e_cascade_target <= target_primaries_energy * (1 + 1e-6)
+                or (e_cascade_target - target_primaries_energy < 0.5)
+            ):
                 raise ValueError(
                     f"Energy deposited in cascades is greater than primary energy: {e_cascade_target} > {target_primaries_energy}\nEvent header: {frame['I3EventHeader']}"
                 )
@@ -121,7 +124,10 @@ class I3Calorimetry(I3Extractor):
                 e_cascade_bkg = self.total_cascade_energy(
                     bkg_tree, bkg_primaries
                 )
-                if e_cascade_bkg > bkg_primaries_energy:
+                if not (
+                    e_cascade_bkg <= bkg_primaries_energy * (1 + 1e-6)
+                    or (e_cascade_bkg - bkg_primaries_energy < 0.5)
+                ):
                     raise ValueError(
                         f"Energy deposited in cascades is greater than primary energy: {e_cascade_bkg} > {bkg_primaries_energy}\nEvent header: {frame['I3EventHeader']}"
                     )
@@ -146,7 +152,7 @@ class I3Calorimetry(I3Extractor):
                 or (e_total_target - target_primaries_energy < 0.5)
             ):
                 raise ValueError(
-                    "Total energy on entrance is greater than primary energy\n"
+                    "Total energy is greater than primary energy\n"
                     f"Total energy: {e_total_target}\n"
                     f"Primary energy: {target_primaries_energy}\n"
                     f"Track deposited energy: {e_track_target}\n"
